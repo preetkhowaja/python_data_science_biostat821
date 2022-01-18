bbf07eb7e1cc64ada5f01ad25d2ab991f312d649
hb173 <hb173@duke.edu>
2022-01-18 18:28:11 -0500

    Part 1

--- functions.py
+++ functions.py
@@ -2,3 +2,20 @@
 ### Group: Himanshu Raj Bhantana and Preet Khowaja
 
 # this function accepts a txt file and returns a list of lists of integers from that file
+
+def get_data(file):
+    with open (file) as f:
+        LL = []
+
+        for line in f:
+            list1 = line.split()
+            list2=[]
+            for i in range(0,len(list1)):
+                list2.append(int(list1[i]))
+            LL.append(list2)
+        print(LL)
+
+
+get_data('example.txt')
+
+
